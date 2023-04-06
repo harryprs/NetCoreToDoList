e5c99100bc1b0caf4c4f2d58edfe7a10f84cb308
harryprs <harryprs@hotmail.co.uk>
2023-04-06 16:12:03 +0100

    Use Developer Exception Page

--- ToDo List/Program.cs
+++ ToDo List/Program.cs
@@ -171,6 +171,9 @@ namespace ToDo_List
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }*/
+            
+            app.UseDeveloperExceptionPage();
+            app.UseHsts();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
