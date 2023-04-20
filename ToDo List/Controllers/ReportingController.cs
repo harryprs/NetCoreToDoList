9ab74142a6c22767e8ea642e170e97d58c984114
harryprs <harryprs@hotmail.co.uk>
2023-04-20 11:05:55 +0100

    Added a ReportingData model, added new reporting data

--- ToDo List/Controllers/ReportingController.cs
+++ ToDo List/Controllers/ReportingController.cs
@@ -28,7 +28,7 @@ namespace ToDo_List.Controllers
             _context = context;
             toDoRepo = new ToDoRepository(context);
         }
-        
+
         // GET: Reporting/OverallReport
         public async Task<IActionResult> OverallReport()
         {
@@ -36,50 +36,97 @@ namespace ToDo_List.Controllers
             var toDoLists = await toDoRepo.GetAllToDoListsByUserId(userId);
 
             var totalListsCompleted = toDoLists.Where(l => l.IsFinished).Count();
-            
+
             var listItems = toDoLists.SelectMany(l => l.ListItems).ToList();
             int totalTimeSpent = 0;
             int? totalEstimatedTime = 0;
-            foreach (var item in listItems)
+            int listItemsCompletedCount = 0;
+            int listItemsInProgressCount = 0;
+            int listItemsNotStartedCount = 0;
+            foreach(var item in listItems)
             {
                 totalTimeSpent += item.TimeSpent;
                 if (totalEstimatedTime != null)
                 {
                     totalEstimatedTime += item.EstimatedTime;
                 }
+                switch((int)item.Progress)
+                {
+                    case 1:
+                        listItemsNotStartedCount++;
+                        break;
+                    case 2:
+                        listItemsInProgressCount++;
+                        break;
+                    case 3:
+                        listItemsCompletedCount++;
+                        break;
+                }
             }
-
-            var dailyCompletedCount = new Dictionary<double, double>();
+            // Get the count of ToDoLists completed, indexed by date completed
+            var dailyCompletedListCount = new Dictionary<double, double>();
             foreach(var item in toDoLists)
             {
-                if (item.IsFinished)
+                if(item.IsFinished)
                 {
                     // Convert DateTime to Javascript timestamp, ignore time
                     var date = item.DateTimeListFinished.Value.Date.ToUniversalTime().Subtract(
                         new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
-                    if (!dailyCompletedCount.ContainsKey(date))
+                    if(!dailyCompletedListCount.ContainsKey(date))
                     {
-                        dailyCompletedCount[date] = 1;
+                        dailyCompletedListCount[date] = 1;
                     }
                     else
                     {
-                        dailyCompletedCount[date] += 1;
+                        dailyCompletedListCount[date] += 1;
                     }
                 }
             }
 
-            List<DataPoint> dataPoints = new List<DataPoint>();
-            foreach(var dictItem in dailyCompletedCount)
+            List<DataPoint> listsDataPoints = new List<DataPoint>();
+            foreach(var dictItem in dailyCompletedListCount)
             {
-                dataPoints.Add(new DataPoint(dictItem.Key, dictItem.Value));
+                listsDataPoints.Add(new DataPoint(dictItem.Key, dictItem.Value));
             }
 
-            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
-            ViewBag.TotalListsCompleted = totalListsCompleted;
-            ViewBag.TotalTimeSpent = totalTimeSpent;
-            ViewBag.TotalEstimatedTime = totalEstimatedTime;
+            // Get the count of ListItems completed, indexed by daate completed
+            var dailyCompletedItemCount = new Dictionary<double, double>();
+            foreach (var item in listItems)
+            {
+                if ((int)item.Progress == 3)
+                {
+                    var date = item.DateTimeItemFinished.Value.Date.ToUniversalTime().Subtract(
+                            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+                    if (!dailyCompletedItemCount.ContainsKey(date))
+                    {
+                        dailyCompletedItemCount[date] = 1;
+                    }
+                    else
+                    {
+                        dailyCompletedItemCount[date] += 1;
+                    }
+                }
+            }
 
-            return View();
+            List<DataPoint> listItemsDataPoints = new List<DataPoint>();
+            foreach(var dictItem in dailyCompletedItemCount)
+            {
+                listItemsDataPoints.Add(new DataPoint(dictItem.Key, dictItem.Value));
+            }
+
+            ReportingData reportingData = new ReportingData()
+            {
+                ListsCompletedDataPoint = JsonConvert.SerializeObject(listsDataPoints),
+                ListItemsCompletedDataPoint = JsonConvert.SerializeObject(listItemsDataPoints),
+                TotalListsCompleted = totalListsCompleted,
+                TotalTimeSpent = totalTimeSpent,
+                TotalEstimatedTime = totalEstimatedTime,
+                ListItemsCompletedCount = listItemsCompletedCount,
+                ListItemsInProgressCount = listItemsInProgressCount,
+                ListItemsNotStartedCount = listItemsNotStartedCount
+            };
+
+            return View(reportingData);
         }
 
         public int GetUserIdFromClaims()
---
+++ ToDo List/Models/ReportingData.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDo_List.Models
+{
+    public class ReportingData
+    {
+        public string ListsCompletedDataPoint { get; set; }
+        public string ListItemsCompletedDataPoint { get; set; }
+        public int TotalListsCompleted { get; set; }
+        public int TotalTimeSpent { get; set; }
+        public int? TotalEstimatedTime { get; set; }
+        public int ListItemsNotStartedCount { get; set; }
+        public int ListItemsInProgressCount { get; set; }
+        public int ListItemsCompletedCount { get; set; }
+    }
+}
